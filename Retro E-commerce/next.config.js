9b02c01174f3c04e5950562bd933b86d16329be9
nadamaged <nadamaged200@gmail.com>
2026-04-28 16:38:00 +0300

    rempve webpack

--- Retro E-commerce/next.config.js
+++ Retro E-commerce/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.cache = false
-    return config
-  },
 }
 
 module.exports = nextConfig
